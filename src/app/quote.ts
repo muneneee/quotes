b2c9302d33a8d78a585c9328963f11b8b5db5c34
[muneneee] <[lukekevin39@gmail.com]>
2020-04-02 12:56:11 +0300

    created a pipe for date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-    showDescription: boolean;
-    constructor(public id: number,public title: string,public description: string){
+    public showDescription: boolean;
+    constructor(public id: number,public title: string,public description: string, public completeDate){
       this.showDescription=false;
     }
 };

2131ae091ab8f8d94a0c32c009a95d450148826c
[muneneee] <[lukekevin39@gmail.com]>
2020-04-03 17:54:34 +0300

    created custom directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private) { }
+  constructor(private elem:ElementRef) { }
 
 }

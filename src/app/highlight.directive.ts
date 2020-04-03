c40821647f6e8652b0c73ab40ce5869d3b6c5827
[muneneee] <[lukekevin39@gmail.com]>
2020-04-03 17:57:42 +0300

    created custom directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem:ElementRef) { 
-    this.elem.nativeElement,style.
+    this.elem.nativeElement.style.textDecoration=
   }
 
 }
